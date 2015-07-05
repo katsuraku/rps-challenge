1f8daf38f2ffcecee150d2a36986e8c16b7d28e7
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-05 17:23:52 +0100

    game knows result of round. need to flesh out all combinations of wins/draws.

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require_relative 'player'
 require_relative 'computer'
 class Game
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :player1_choice, :player2_choice
 
   def initialize(playerclass)
     @player1 = Player.new
@@ -10,10 +10,16 @@ class Game
   end
 
   def player1_choice
-    self.player1.choice
+    @player1_choice = self.player1.choice
   end
 
   def player2_choice
-    self.player2.choice
+    @player2_choice = self.player2.choice
+  end
+
+  def result
+    if (player1_choice == 'rock') && (player2_choice == 'scissors')
+      return 'Player 1 won!'
+    end
   end
 end
