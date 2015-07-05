689adafaabcc61ff9d99d5034c0fcdc5f9a1c64d
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-05 16:56:10 +0100

    adds test for game to know what each player has chosen

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,12 @@ describe Game do
     game = Game.new(Computer)
     expect(game).to respond_to(:player2)
   end
+
+  it 'knows what each player\'s choice is' do
+    game = Game.new(Player)
+    game.player1.choose('rock')
+    game.player2.choose('paper')
+    expect(game.player1_choice).to eq 'rock'
+    expect(game.player2_choice).to eq 'paper'
+  end
 end
