3f558aa3e17b1ec9059f7ea079c60733d717d552
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-05 16:43:13 +0100

    test for player to have a choice attribute. plan to feed into gameplay methods

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,4 +5,8 @@ describe Player do
   it 'can choose rock, paper or scissors' do
     expect(subject).to respond_to(:choose).with(1).argument
   end
+
+  it 'has a choice' do
+    expect(subject).to respond_to(:choice)
+  end
 end
